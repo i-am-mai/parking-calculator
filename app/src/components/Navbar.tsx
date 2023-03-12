dc704e36c2460e4e8edaa84d6ebf0303dc877d80
Mai Tran <mai.t.tran1743@gmail.com>
2023-03-12 16:05:53 -0400

    Initialize app

---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../logo.svg'
+
+
+export default function renderNavbar() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    Parking Calculator
+                </Navbar.Brand>
+                <Nav>
+                    <Nav.Link href="/about" className="justify-content-end">About</Nav.Link>
+                    <Nav.Link href="https://github.com/i-am-mai/parking">Contribute</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
