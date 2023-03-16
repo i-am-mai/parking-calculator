f5ef09175d977a8bef985b2327f5264a0c8fc285
Mai Tran <mai.t.tran1743@gmail.com>
2023-03-16 16:13:09 -0400

    Add loading animation

--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -1,29 +1,44 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
 import SearchResults from './SearchResults';
 import "./Sidebar.css"
 
 export default function Sidebar() {
     const [data, setData] = useState([]);
+    const [spinner, setSpinner] = useState(false);
+
 
     function handleSubmit(event : React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        setSpinner(true);
         let query = (event.target as HTMLFormElement).search.value;
         console.log(query);
 
         fetch("/search?q=" + query)
             .then(res => res.json())
-            .then(data => setData(data));
+            .then(data => setData(data))
+            .then(() => setSpinner(false));
+    }
+
+    let results;
+
+    if (spinner) {
+        results = <Spinner animation="border" id="spinner"></Spinner>
+    }
+    else {
+        results = <SearchResults results={data} />
+
     }
 
     return (
         <div className="sidebar">
             <form onSubmit={handleSubmit} className="input">
                 <Form.Control name="search" placeholder="ex. New York, NY"></Form.Control>
-                <Button variant="secondary">Search</Button>    
+                <Button variant="secondary" type="submit">Search</Button>    
             </form>
-            <SearchResults results={data} />
+            {results}
             <Button variant="primary" className="calculate">Calculate parking area</Button>
         </div>
     );
