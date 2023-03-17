b34c86e0dd28a44701f16fb1e4faa64477f1cac4
Mai Tran <mai.t.tran1743@gmail.com>
2023-03-16 20:06:40 -0400

    Make navbar responsive

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../logo.svg'
@@ -8,22 +7,23 @@ import './Navbar.css'
 export default function renderNavbar() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" className="header">
-            <Container>
-                <Navbar.Brand href="/">
-                    <img
-                        alt=""
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    Parking Calculator
-                </Navbar.Brand>
+            <Navbar.Brand href="/" id="brand">
+                <img
+                    alt=""
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                Parking Calculator
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                 <Nav>
-                    <Nav.Link href="/about" className="justify-content-end">About</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="https://github.com/i-am-mai/parking">Contribute</Nav.Link>
                 </Nav>
-            </Container>
+            </Navbar.Collapse>
         </Navbar>
     );
 }
